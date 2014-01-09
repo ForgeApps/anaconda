4c1eee2a555303a66a2ca7572d9e10a3ff3090ff
jeffmcfadden <jeff@forgeapps.com>
2014-01-09 11:31:11 -0700

    Require external dependencies to get the assets to load properly.

---
+++ app/assets/javascripts/anaconda.js
@@ -0,0 +1,5 @@
+//= require jquery-fileupload/basic
+//= require jquery-fileupload/vendor/tmpl
+//= require md5-min
+//= require dlog.js
+//= require anaconda_uploader
--- lib/anaconda/engine.rb
+++ lib/anaconda/engine.rb
@@ -1,6 +1,8 @@
 module Anaconda
   module Rails
     class Engine < ::Rails::Engine
+      require 'jquery-fileupload-rails'
+      require 'javascript_dlog-rails'
     end
   end
 end
